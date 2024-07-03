6ba60adf87a9c365d12ff679f7237d965ea2ea1e
Florian Schmidt <flo.schmidt10@gmail.com>
2024-07-03 23:13:17 +0200

    make build-docker.sh accept an argument which image should be rebuilt

--- build-docker.sh
+++ build-docker.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
-# replace the --tag appropriately
-# use --load instead of --push to use the image locally
 
-# only needs to be ran once
+# Function to build and push stuwe version
+build_stuwe() {
+  docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --tag flschmidt/stuwe-telegram-rs --target stuwe-telegram-rs --push .
+}
+
+# Function to build and push mensi version
+build_mensi() {
+  docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --tag flschmidt/mensi-telegram-rs --target mensi-telegram-rs --push .
+}
+
+# only needs to be run once
 docker buildx create --name mybuilder --use
 
-# build and push stuwe version
-docker buildx build \
---platform linux/amd64,linux/arm64 \
---tag flschmidt/stuwe-telegram-rs --target stuwe-telegram-rs --push .
-
-# build and push mensi version (reuses builder)
-docker buildx build \
---platform linux/amd64,linux/arm64 \
---tag flschmidt/mensi-telegram-rs --target mensi-telegram-rs --push .
-
+case $1 in
+  stuwe)
+    build_stuwe
+    ;;
+  mensi)
+    build_mensi
+    ;;
+  all)
+    build_stuwe
+    build_mensi
+    ;;
+  *)
+    echo "Error: Invalid argument. Use 'stuwe', 'mensi', or 'all'."
+    exit 1
+    ;;
+esac
